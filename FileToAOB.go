24d7697933194bf49db8cd041f0b392a460f0b16
APPDIZ <38929104+apr3ndi5@users.noreply.github.com>
2021-02-16 03:34:10 -0300

    First Commit

---
+++ FileToAOB.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+/*FileName file name to export*/
+var FileName string = os.Args[1]
+
+var file string = GetRealtivePath() + `\` + FileName
+var path string = GetRealtivePath() + `\file.txt`
+
+func main() {
+	FileBuffer := Read(file)
+	CreateFile()
+	WriteFile(FileBuffer)
+}
+
+/*GetRealtivePath rola que tal*/
+func GetRealtivePath() (path string) {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	path = filepath.Dir(ex)
+	return
+}
+
+/*Read file buffer*/
+func Read(file string) (FileBuffer []byte) {
+	FileBuffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Print(err)
+	}
+	return
+}
+
+/*CreateFile create file*/
+func CreateFile() {
+
+	// detect if file exists
+	var _, err = os.Stat(path)
+
+	// create file if not exists
+	if os.IsNotExist(err) {
+		var file, err = os.Create(path)
+		if isError(err) {
+			return
+		}
+		defer file.Close()
+	}
+
+	fmt.Println("[] done creating file", path)
+}
+
+/*WriteFile write file*/
+func WriteFile(p []byte) {
+
+	// open file using READ & WRITE permission
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+
+	mystring := fmt.Sprintf("%d", p)
+
+	if isError(err) {
+		return
+	}
+	defer file.Close()
+
+	c := strings.ReplaceAll(mystring, " ", ", ")
+	c = strings.ReplaceAll(c, "[", "{")
+	c = strings.ReplaceAll(c, "]", "}")
+	// write into file
+	_, err = file.WriteString(c)
+	if isError(err) {
+		return
+	}
+
+	// save changes
+	err = file.Sync()
+	if isError(err) {
+		return
+	}
+
+	fmt.Println("[] done writing to file")
+}
+
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return (err != nil)
+}
